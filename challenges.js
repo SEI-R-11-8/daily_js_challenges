298b2a1a2657a5e2c0d562ced0ed2e661b1226d4
jjvenable <jjvenable@gmail.com>
2021-11-15 09:34:57 -0500

    complete through 00

--- challenges.js
+++ challenges.js
@@ -7,12 +7,19 @@ Prompt:
 
 Write a function called sayHello that returns the string "Hello!".
 
-Examples:
+Examples: ((What should show up))
 
 sayHello() //=> Hello!
 -----------------------------------------------------------------*/
 // Your solution for 00-sayHello (example) here:
-function sayHello() {}
+function sayHello() {
+  return 'Hello!';
+}
+/* or 
+const sayHello - => {
+  return "Hello!"
+}
+*/
 /*-----------------------------------------------------------------
 Challenge: 01-addOne
 
@@ -721,4 +728,4 @@ module.exports = {
   gridTrip,
   addChecker,
   totalTaskTime
-}
+};

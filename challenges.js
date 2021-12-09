230e65326e2e57a5f50cacf4cce27d84f3d96ce6
RicoWilliams <ricowilliams@protonmail.com>
2021-12-09 08:00:59 -0700

    #4 complete

--- challenges.js
+++ challenges.js
@@ -90,7 +90,8 @@ Difficulty: Basic
 
 Prompt:
 
-- Write a function called addList that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+- Write a function called addList that accepts any quantity of numbers as arguments, 
+adds them together and returns the resulting sum.
 - Assume all parameters will be numbers.
 - If called with no arguments, return 0 (zero).
 
@@ -101,7 +102,13 @@ add(1,50,1.23) //=> 52.23
 add(7,-12) //=> -5
 -----------------------------------------------------------------*/
 // Your solution for 04-addList here:
-function addList() {}
+function addList() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments;
+  }
+  return sum;
+}
 /*-----------------------------------------------------------------
 Challenge: 05-computeRemainder
 

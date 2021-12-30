49c60c07b364360d2cd95065d48ff98198f5a110
Christopher Bowers <c_bowers@me.com>
2021-12-30 12:32:54 -0500

    Solved 12

--- challenges.js
+++ challenges.js
@@ -310,7 +310,21 @@ hammingDistance('!!!!', '****'); //=> 4
 hammingDistance('abc', 'ab'); //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 12-hammingDistance here:
-function hammingDistance(str1, str2) {}
+function hammingDistance(str1, str2) {
+  if (str1.length !== str2.length ) {
+    return NaN
+  }
+  
+  let dist = 0
+  
+  for (let i = 0; i < str1.length; i += 1) {
+    if (str1[i] !== str2[i]) {
+      dist += 1
+    }
+   }
+   
+   return dist
+}
 /*-----------------------------------------------------------------
 Challenge: 13-mumble
 

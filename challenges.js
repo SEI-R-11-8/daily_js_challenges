3e7f105a34b75b9ced104c9db06094583504df24
janemzwangnj <janemzwangnj@gmail.com>
2022-01-29 17:11:10 -0500

    20 done

--- challenges.js
+++ challenges.js
@@ -576,7 +576,7 @@ function isPrime(n) {
   if (n < 2 || !Number.isInteger(n)) {
     return false;
   }
-  for (let i = 2; i < n / 2; i++) {
+  for (let i = 2; i <= n / 2; i++) {
     if (Number.isInteger(n / i)) {
       return false;
     }

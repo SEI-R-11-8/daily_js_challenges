c477bfe571020673145473ebc7b5add56518ef12
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-02-04 23:27:50 -0500

    c15

--- challenges.js
+++ challenges.js
@@ -431,7 +431,11 @@ mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  //=> {a: 1, b: 2, c: 3, d: 4}
 mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44}
 -----------------------------------------------------------------*/
 // Your solution for 15-mergeObjects here:
-function mergeObjects(obj1, obj2) {}
+function mergeObjects(obj1, obj2) {
+  for (i = 0; i < arguments.length; i++) {
+    Object.assign(obj1, arguments[i]);
+  }
+  return obj1;
 /*-----------------------------------------------------------------
 Challenge: 16-findHighestPriced
 
